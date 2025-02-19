c7821caf98717247dd34af663b34dfd81a0ce7c9
elnurxo <elnurykh@code.edu.az>
2025-02-19 17:45:56 +0400

    js introduction

---
+++ 02. js introduction/operators.js
@@ -0,0 +1,42 @@
+let employee1 = {
+  fullName: "John Doe",
+  salary: 7500,
+  position: "frontend developer",
+  skills: ["html", "css", "js", "git", "react"],
+  age: 26,
+  experienceYear: 4,
+  isMarried: false,
+};
+
+// if(employee1.experienceYear > 5){
+//     employee1.salary += employee1.salary * 20 / 100;
+//     console.log(employee1);
+// }
+// else{
+//     console.log('not enough experience for salary update!');
+// }
+
+let employee2 = {
+  fullName: "Beck Adams",
+  salary: 1700,
+  position: "junior frontend developer",
+  skills: ["html", "css", "js", "git"],
+  age: 19,
+  experienceYear: 2,
+  isMarried: true,
+};
+
+// let fullName = "Eli Eliyev";
+// let age = 21;
+// let balance = 24;
+// let ticketPrice = 5.5;
+// let ticketCount = 1;
+// let ageRestriction = 18;
+
+// if (age >= ageRestriction) {
+//   console.log("Welcome to CinemaPlus!");
+//   balance -= ticketPrice * ticketCount;
+//   console.log("current balance is: ", balance);
+// } else {
+//   console.log("Get boyu gelersen!");
+// }
